2936451e2635db88bf07b385086462af03e7e2a0
Michele Tagliabue Mac <mikitg.michele@gmail.com>
2019-02-02 21:07:14 +0100

    Iniziata (bozza) pagina di find match

--- lib/pages/multiplayer/find_match.dart
+++ lib/pages/multiplayer/find_match.dart
@@ -11,11 +11,53 @@ class LetsMemoryFindMatch extends StatefulWidget {
 }
 
 class _LetsMemoryFindMatchState extends State<LetsMemoryFindMatch> {
+  String searchQuery;
+
+  @override
+  void initState() {
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return LetsMemoryBackground(
       children: <Widget>[
-        Text("Cerca avversario",style: LetsmemoryStyles.mainTitle),
+        Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: LetsMemoryDimensions.standardCard/2
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Cerca avversario",
+                style: LetsmemoryStyles.mainTitle,
+                textAlign: TextAlign.center
+              ),
+              Padding(padding:EdgeInsets.only(top:10)),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                autocorrect: false,
+                onChanged: (text) {
+                  this.searchQuery = text.trim();
+                },
+                autofocus: false,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(20.0),
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius:BorderRadius.circular(LetsMemoryDimensions.cardRadius)
+                  ),
+                  hintText: "Username...",
+                  suffixIcon: Icon(
+                    Icons.search,
+                    size: LetsMemoryDimensions.standardCard,
+                  ),
+                ),
+              ),
+            ],
+              
+          ),
+        ),
       ]
     );
   }
